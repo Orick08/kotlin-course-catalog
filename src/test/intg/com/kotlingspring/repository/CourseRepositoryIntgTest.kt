f33c1c283657a836385471b7619ab931c0922241
erick.amezcua <erick.amezcua@points.com>
2023-03-21 10:54:02 -0600

    Adds parametized test example

--- src/test/intg/com/kotlingspring/repository/CourseRepositoryIntgTest.kt
+++ src/test/intg/com/kotlingspring/repository/CourseRepositoryIntgTest.kt
@@ -4,9 +4,13 @@ import com.kotlingspring.util.courseEntityList
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
+import java.util.stream.Stream
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -38,5 +42,20 @@ class CourseRepositoryIntgTest {
         Assertions.assertEquals(1, courses.size)
     }
 
+    @ParameterizedTest
+    @MethodSource("courseAndSize")
+    fun findCoursesByName_approach2(name: String, expectedSize: Int){
+        val courses = courseRepository.findCoursesByName(name)
+        println("courses : $courses")
 
+        Assertions.assertEquals(expectedSize, courses.size)
+    }
+
+    companion object {
+        @JvmStatic
+        fun courseAndSize(): Stream<Arguments>{
+            return Stream.of(Arguments.arguments("Micronaut", 1),
+                Arguments.arguments("Java", 1))
+        }
+    }
 }
